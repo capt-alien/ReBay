9d761029b25536e4496aa55696bc15d7eb4ba8a6
capt-alien <eric.botcher@presidio.edu>
2019-03-20 17:17:02 -0700

    user resoruce created

--- app.py
+++ app.py
@@ -31,7 +31,56 @@ def create_tables():
 # Auth (turn on later)
 # jwt = JWT(app, authenticate, identity)
 
+class UserModel(db.Model):
+    __tablename__='users'
 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+# *******USER RESOURCE*****
+class UserRegister(Resource):
+    parser=reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be blank."
+                        )
+
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400
+
+        user = UserModel(data['username'], data['password'])
+        user.save_to_db()
+
+        return {"message": "User created successfully."}, 201
+
+# *****Routes*******
 # ITEM
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
@@ -41,7 +90,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 # User
-# api.add_resource(UserRegister, '/register')
+api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
